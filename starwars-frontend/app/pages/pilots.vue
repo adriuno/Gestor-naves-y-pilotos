eb87fc2780852003437142b972779aab0ff1a283
adriuno <gmadrian94@gmail.com>
2025-05-07 12:10:28 +0200

    Sanctum ya funciona, login bien, falta logout y registro, y pantalla de carga de pilotos 3.0

---
+++ starwars-frontend/app/pages/pilots.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="relative min-h-screen">
+      <!-- Fondo -->
+      <div class="absolute inset-0">
+        <img src="/images/sw3.jpg" alt="galaxy" class="w-full h-full object-cover brightness-75">
+      </div>
+   
+      <div class="relative z-10">
+        <AppHeader />
+        <main class="p-8  mx-auto  bg-black/70 rounded-xl text-center">
+          <p class="mt-4 text-4xl text-yellow-300 custom-starwars">En una galaxia muy, muy lejana...</p>
+          <br><br>
+          <p class="text-3xl text-yellow-300 custom-starwars">¡Bienvenido al gestor de</p>
+          <p class="text-3xl text-yellow-300 custom-starwars">naves y pilotos!</p> <br>
+  
+          <!-- <CardsPilots  />  -->
+           <CardsPilots />
+        </main>
+        
+      </div>
+    </div>
+  </template>
+  
